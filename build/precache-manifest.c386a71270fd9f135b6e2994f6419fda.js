117981d90346cd606ac4b461ef37ce42b8480214
Nick <nhorton04@gmail.com>
2018-11-27 21:29:33 -0800

    backend setup

---
+++ build/precache-manifest.c386a71270fd9f135b6e2994f6419fda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "226d9130f36b39b79698",
+    "url": "/static/js/main.226d9130.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "226d9130f36b39b79698",
+    "url": "/static/css/main.19ded8a4.chunk.css"
+  },
+  {
+    "revision": "0bca2ac623a68b2d2508a8338dce5b6c",
+    "url": "/index.html"
+  }
+];
