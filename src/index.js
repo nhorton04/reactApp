ae686487dd05371ddec0fd2faa99552b7e6c7a2b
Nick <nhorton04@gmail.com>
2018-11-27 21:55:09 -0800

    add pointer to comment box

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import CommentBox from './CommentBox';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<CommentBox /> document.getElementById('root'));
+
+registerServiceWorker();
 
 function Square(props) {
   return (
